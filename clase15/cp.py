a678e4ad7ed4e5fd6c496a16da7927c6bae5c037
Diego Serrano <diegojserrano@gmail.com>
2023-04-10 20:52:58 -0300

    Clase 15

--- clase15/cp.py
+++ clase15/cp.py
@@ -4,8 +4,7 @@
 # Luego de la carga:
 # Mostrar todo el contenido de la lista
 # Ingresada una provincia, mostrar todas las localidades de esa provincia ordenadas por numero
-# Ingresar un texto y mostrar todas las localidades cuyo nombre incluya dicho texto, ordenadas por provincia y nombre
-# Por cada número mostrar la cantidad de repeticiones
+
 
 
 # Tomar la idea del programa clase14/personas.py
@@ -27,25 +26,37 @@ def provincia(localidad): return localidad["provincia"]
 def numero(localidad): return localidad["numero"]
 def nombre(localidad): return localidad["nombre"]
 
+def prov_nombre(localidad): return provincia(localidad) + nombre(localidad)
 def cargar_localidades(nombre_archivo):
-
     archivo = open(nombre_archivo, "rt", encoding="ISO-8859-1")
-    #localidades = []
-
-    #while True:
-    #    linea = archivo.readline()
-    #    if linea == "": break
-    #    localidades.append(crear_localidad(linea))
-
-    #return localidades
-
     localidades = [crear_localidad(linea) for linea in archivo.readlines()]
     archivo.close()
     return localidades
 
+def grabar_localidades(nombre_archivo, lista):
+    archivo = open(nombre_archivo, "wt")
+
+    for l in lista:
+        archivo.write(f"{provincia(l)},{numero(l)},{nombre(l)}\n")
+
+    archivo.close()
+
+
 def listar_localidades(lista):
     for l in lista:
         print(f"| {provincia(l)} | {numero(l)} | {nombre(l):<60} |")
+
+def contar_por_numero(lista):
+    numeros = dict()
+
+    for l in lista:
+        n = numero(l)
+        if n in numeros:
+            numeros[n] += 1
+        else:
+            numeros[n] = 1
+
+    return numeros
 
 
 todas = cargar_localidades("CP.txt")
@@ -54,3 +65,14 @@ todas = cargar_localidades("CP.txt")
 prov = input("Ingrese el código de la provincia (X, H, K): ")
 encontradas = sorted([l for l in todas if provincia(l) == prov], key=numero)
 listar_localidades(encontradas)
+# Ingresar un texto y mostrar todas las localidades cuyo nombre incluya dicho texto, ordenadas por provincia y nombre
+buscada = input("Ingrese un nombre para buscar: ")
+encontradas = sorted([l for l in todas if buscada in nombre(l)], key=prov_nombre)
+listar_localidades(encontradas)
+nombre_archivo = buscada + ".csv"
+grabar_localidades(nombre_archivo, encontradas)
+
+# Por cada número mostrar la cantidad de repeticiones (pensar en un diccionario en el que la clave sea el numero)
+cantidades = contar_por_numero(todas)
+for n, c in cantidades.items():
+    print(f"Código: {n}: {c} localidades")
